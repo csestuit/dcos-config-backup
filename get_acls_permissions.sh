bf7a49153f915f296a5dce03878a7913ddfecbd8
Cloud User <centos@ip-172-31-40-89.us-west-2.compute.internal>
2016-10-08 18:07:31 +0000

    get_acls_permissions.sh is OK

--- get_acls_permissions.sh
+++ get_acls_permissions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# Get a set of ACLs (Permission Rules )configured in a running DC/OS cluster, and save
+# Get a set of permission rules associated with the ACLs in the system
 #them to a file in raw JSON format for backup and restore purposes.
 #These can be restored into a cluster with the accompanying 
-#"post_acls.sh" script.
+#"post_acls_permissions.sh" script.
 
 #reference: 
 #https://docs.mesosphere.com/1.8/administration/id-and-access-mgt/iam-api/#!/permissions/get_acls
@@ -27,19 +27,38 @@ else
   echo "** ERROR: Configuration not found. Please run ./run.sh first"
 fi
 
-#get ACLs from cluster
-ACLS=$(curl \
+#Reset contents of ACLS_PERMISSIONS_FILE
+touch $ACLS_PERMISSIONS_FILE
+echo "" > $ACLS_PERMISSIONS_FILE
+
+#loop through the ACLs in the ACLS_FILE and get their respective permissions
+#then save each permission to ACLS_PERMISSIONS_FILE
+jq -r '.array|keys[]' $ACLS_FILE | while read key; do
+
+        echo -e "*** Loading ACL "$key" ..."
+        ACL=$(jq ".array[$key]" $ACLS_FILE)
+	_RID=$(echo $ACL | jq -r ".rid")
+    	#get the associated URL which gives the permissions
+	URL=$(echo $ACL | jq -r ".url")
+	echo "** DEBUG: URL is :"$URL
+	#query the ACL's URL to get the associated permissions
+        echo -e "*** Saving PERMISSIONS for rule "key": "$_RID" ..."
+        PERMISSION=$( curl \
 -H "Content-Type:application/json" \
 -H "Authorization: token=$TOKEN" \
+-d "$BODY" \
 -X GET \
-http://$DCOS_IP/acs/api/v1/acls)
+http://$DCOS_IP/$URL/permissions )
+        sleep 1
 
-#save to file
-touch $ACLS_FILE
-echo $ACLS > $ACLS_FILE
+	#Attach permission to end of file
+	echo -e "** DEBUG: Received permission is: "
+	echo $PERMISSION | jq
+	echo $PERMISSION >> $ACLS_PERMISSIONS_FILE
 
-#debug
-echo "** ACLs: "
-echo $ACLS | jq
+	#DEBUG: show contents of file to stdout to check progress
+	echo "*** DEBUG current contents of file after RULE: "$_RID
+	cat $ACLS_PERMISSIONS_FILE
+done
 
 echo "Done."
