6ae8a5d35f53b974effe308bcfe6b925a34cc063
Cloud User <centos@ip-172-31-40-89.us-west-2.compute.internal>
2016-10-09 00:30:58 +0000

    clean up style GAPA

--- get_acls_permissions_actions.sh
+++ get_acls_permissions_actions.sh
@@ -36,9 +36,9 @@ echo "{ "\"array"\": [" > $ACLS_PERMISSIONS_ACTIONS_FILE
 jq -r '.array|keys[]' $ACLS_PERMISSIONS_FILE | while read key; do
 
 	echo -e "*** Loading Permission "$key" ..."
-	_RID=$(jq ".array[$key].rid" $ACLS_PERMISSIONS_FILE)
+	_RID=$( jq ".array[$key].rid" $ACLS_PERMISSIONS_FILE )
 	echo "** DEBUG: Permission number "$key" is associated with rule ID"$_RID
-	PERMISSION=$(jq ".array[$key].permission" $ACLS_PERMISSIONS_FILE) 
+	PERMISSION=$( jq ".array[$key].permission" $ACLS_PERMISSIONS_FILE ) 
 	echo "** DEBUG: Permission number "$key" of rule "$_RID" is "$PERMISSION
 
 	#check whether it's a USER or GROUP rule
@@ -48,26 +48,26 @@ jq -r '.array|keys[]' $ACLS_PERMISSIONS_FILE | while read key; do
 	echo "** DEBUG: Group for rule "$_RID" is "$_GROUP
 
 	#if the user array is empty - length 0
-	if [ $(echo $_USER | jq '. | length' ) == 0 ]; then
+	if [ $( echo $_USER | jq '. | length' ) == 0 ]; then
 		
-		if [ $(echo $_GROUP | jq '. | length' ) == 0 ]; then
+		if [ $( echo $_GROUP | jq '. | length' ) == 0 ]; then
 
-			#system/services/ops rule
+			#SYSTEM/services/ops rule
 			#have no ACTIONS so we just log and keep going
 			echo "** DEBUG: SYSTEM/service/ops rule"
 		
 		else
 			
-			#group rule
+			#GROUP rule
 			echo "** DEBUG: GROUP Rule"
 			#Groups includes the .Actions array, need to loop through it
 			
-			ACTIONS=$(echo $_GROUP | jq ".actions")
-			$(echo $ACTIONS | jq -r '.actions|keys[]') | while read key; do	
+			ACTIONS=$( echo $_GROUP | jq ".actions" )
+			$( echo $ACTIONS | jq -r '.actions|keys[]' ) | while read key; do	
 			
-				NAME=$(echo $ACTIONS | jq -r ".actions[$key].name")
+				NAME=$( echo $ACTIONS | jq -r ".actions[$key].name" )
 				echo "** DEBUG: Name is: "$NAME
-				URL=$(echo $ACTIONS | jq -r ".actions[$key].url")
+				URL=$( echo $ACTIONS | jq -r ".actions[$key].url" )
 				echo "** DEBUG: $URL is: "$URL
 				#GET ACTION value from the URL and store it
 				echo -e "*** Getting ACTION for rule "key": "$_RID" ..."
@@ -85,6 +85,7 @@ http://$DCOS_IP/$URL )
 				BODY=" { "\"rid"\": "\"$_RID"\", "\"gid"\": "\"$_GID"\", "\"action"\": "
 				BODY+="\"$ACTION"\"
 				BODY+="},"
+				echo -e "** DEBUG: BODY is: "$BODY
 				#once the action has a BODY with and index, save it
 				echo $BODY >> $ACLS_PERMISSIONS_ACTIONS_FILE
 
@@ -98,17 +99,17 @@ http://$DCOS_IP/$URL )
 	
 	else
 		
-		#user rule
+		#USER rule
 		echo "** DEBUG: USER Rule"
-		_UID=$(echo $_USER | jq -r ".uid")
+		_UID=$( echo $_USER | jq -r ".uid" )
 		echo "** DEBUG: USER is: "$_UID
 		#Groups includes the .Actions array, need to loop through it
-		ACTIONS=$(echo $_USER | jq ".actions")        
-		$(echo $ACTIONS | jq -r '.actions|keys[]') | while read key; do
+		ACTIONS=$( echo $_USER | jq ".actions" )        
+		$( echo $ACTIONS | jq -r '.actions|keys[]' ) | while read key; do
 																
-			NAME=$(echo $ACTIONS | jq -r ".actions[$key].name")
+			NAME=$( echo $ACTIONS | jq -r ".actions[$key].name" )
 			echo "** DEBUG: Name is: "$NAME
-			URL=$(echo $ACTIONS | jq -r ".actions[$key].url")
+			URL=$( echo $ACTIONS | jq -r ".actions[$key].url" )
 			echo "** DEBUG: $URL is: "$URL
 
 			#GET ACTION value from the URL and store it
@@ -121,7 +122,7 @@ http://$DCOS_IP/$URL )
 http://$DCOS_IP/$URL )	
 			sleep 1
 		
-			echo -e "** DEBUG: Action received isi: "$ACTION
+			echo -e "** DEBUG: Action received is: "$ACTION
 			#Actions dont have an index, so in order to ID them,
 			#embed a first field in each entry with the associated _RID and UID
 			BODY=" { "\"rid"\": "\"$_RID"\", "\"uid"\": "\"$_UID"\", "\"action"\": "
