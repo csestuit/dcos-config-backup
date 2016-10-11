4fdd22b2802f81f279a9229c9eb9f2c55980e283
Fernando Sanchez <fernandosanchezmunoz@gmail.com>
2016-10-10 20:24:07 -0400

    move env to separate file for clarity

---
+++ env.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# env: environment variables used for the functioning of the scripts
+
+# Author: Fernando Sanchez [ fernando at mesosphere.com ]
+#
+# These environment variables define the location and configuration values used 
+# in the rest of the scripts. They're loaded upon running run.sh
+
+#Configurable default values
+DCOS_IP=127.0.0.1
+USERNAME=bootstrapuser
+PASSWORD=deleteme
+DEFAULT_USER_PASSWORD=deleteme
+DEFAULT_USER_SECRET=secret
+WORKING_DIR=$PWD
+
+#not exposed but saved
+#config file is stored hidden in current directory, fixed location
+CONFIG_FILE=$PWD"/.config.json"
+
+#directories
+DATA_DIR=$WORKING_DIR"/data"
+SRC_DIR=$WORKING_DIR"/src"
+BACKUP_DIR=$WORKING_DIR"/backup"
+
+#data files
+USERS_FILE=$DATA_DIR/users.json
+GROUPS_FILE=$DATA_DIR/groups.json
+GROUPS_USERS_FILE=$DATA_DIR/groups_users.json
+ACLS_FILE=$DATA_DIR/acls.json
+ACLS_PERMISSIONS_FILE=$DATA_DIR/acls_permissions.json
+ACLS_PERMISSIONS_ACTIONS_FILE=$DATA_DIR/acls_permissions_actions.json
+EXAMPLE_CONFIG=$BACKUP_DIR/example
+
+#scripts
+GET_USERS=$SRC_DIR"/get_users.sh"
+GET_GROUPS=$SRC_DIR"/get_groups.sh" 
+GET_ACLS=$SRC_DIR"/get_acls.sh"
+GET_ACLS_PERMISSIONS=$SRC_DIR"/get_acls_permissions.sh" 
+GET_ACLS_PERMISSIONS_ACTIONS==$SRC_DIR"/get_acls_permissions_actions.sh"
+POST_USERS=$SRC_DIR"/post_users.sh"
+POST_GROUPS=$SRC_DIR"/post_groups.sh"
+POST_ACLS=$SRC_DIR"/post_acls.sh" 
+POST_ACLS_PERMISSIONS=$SRC_DIR"/post_acls_permissions.sh" 
+POST_ACLS_PERMISSIONS_ACTIONS=$SRC_DIR"/post_acls_permissions_actions.sh"
+
+#formatting env vars
+#clear screen
+CLS='printf "\033c"'
+#pretty colours
+RED='\033[0;31m'
+BLUE='\033[1;34m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+#symbols
+PASS="${GREEN}\xE2\x9C\x93${NC}"
+FAIL="${RED}\xE2\x9C\x97${NC}"
+SKULL="\xE2\x98\xA0"
+
+#state vars for options menu (to check whether things have been done and finished OK)
+#initialized to FAIL (not done)
+GET_USERS_OK=$FAIL
+GET_GROUPS_OK=$FAIL
+GET_ACLS_OK=$FAIL
+GET_ACLS_PERMISSIONS_OK=$FAIL
+GET_ACLS_PERMISSIONS_ACTIONS_OK=$FAIL
+POST_USERS_OK=$FAIL
+POST_GROUPS_OK=$FAIL
+POST_ACLS_OK=$FAIL
+POST_ACLS_PERMISSIONS_OK=$FAIL
+POST_ACLS_PERMISSIONS_ACTIONS_OK=$FAIL
--- run.sh
+++ run.sh
@@ -16,69 +16,8 @@
 # configuration and saves it in JSON format to a fixed, well known location in $PWD
 # hidden  under .config.json
 
-#Configurable default values
-DCOS_IP=127.0.0.1
-USERNAME=bootstrapuser
-PASSWORD=deleteme
-DEFAULT_USER_PASSWORD=deleteme
-DEFAULT_USER_SECRET=secret
-WORKING_DIR=$PWD
-
-#not exposed but saved
-#config file is stored hidden in current directory, fixed location
-CONFIG_FILE=$PWD"/.config.json"
-
-#directories
-DATA_DIR=$WORKING_DIR"/data"
-SRC_DIR=$WORKING_DIR"/src"
-BACKUP_DIR=$WORKING_DIR"/backup"
-
-#data files
-USERS_FILE=$DATA_DIR/users.json
-GROUPS_FILE=$DATA_DIR/groups.json
-GROUPS_USERS_FILE=$DATA_DIR/groups_users.json
-ACLS_FILE=$DATA_DIR/acls.json
-ACLS_PERMISSIONS_FILE=$DATA_DIR/acls_permissions.json
-ACLS_PERMISSIONS_ACTIONS_FILE=$DATA_DIR/acls_permissions_actions.json
-EXAMPLE_CONFIG=$BACKUP_DIR/example
-
-#scripts
-GET_USERS=$SRC_DIR"/get_users.sh"
-GET_GROUPS=$SRC_DIR"/get_groups.sh" 
-GET_ACLS=$SRC_DIR"/get_acls.sh"
-GET_ACLS_PERMISSIONS=$SRC_DIR"/get_acls_permissions.sh" 
-GET_ACLS_PERMISSIONS_ACTIONS==$SRC_DIR"/get_acls_permissions_actions.sh"
-POST_USERS=$SRC_DIR"/post_users.sh"
-POST_GROUPS=$SRC_DIR"/post_groups.sh"
-POST_ACLS=$SRC_DIR"/post_acls.sh" 
-POST_ACLS_PERMISSIONS=$SRC_DIR"/post_acls_permissions.sh" 
-POST_ACLS_PERMISSIONS_ACTIONS=$SRC_DIR"/post_acls_permissions_actions.sh"
-
-#formatting env vars
-#clear screen
-CLS='printf "\033c"'
-#pretty colours
-RED='\033[0;31m'
-BLUE='\033[1;34m'
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
-#symbols
-PASS="${GREEN}\xE2\x9C\x93${NC}"
-FAIL="${RED}\xE2\x9C\x97${NC}"
-SKULL="\xE2\x98\xA0"
-
-#state vars for options menu (to check whether things have been done and finished OK)
-#initialized to FAIL (not done)
-GET_USERS_OK=$FAIL
-GET_GROUPS_OK=$FAIL
-GET_ACLS_OK=$FAIL
-GET_ACLS_PERMISSIONS_OK=$FAIL
-GET_ACLS_PERMISSIONS_ACTIONS_OK=$FAIL
-POST_USERS_OK=$FAIL
-POST_GROUPS_OK=$FAIL
-POST_ACLS_OK=$FAIL
-POST_ACLS_PERMISSIONS_OK=$FAIL
-POST_ACLS_PERMISSIONS_ACTIONS_OK=$FAIL
+#load environment variables
+source ./env.sh
 
 function load_configuration {
 #read configuration if it exists
