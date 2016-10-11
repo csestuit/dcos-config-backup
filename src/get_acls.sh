6e1d003c5f0b33832457a28fc54725da75fa0981
Fernando Sanchez <fernandosanchezmunoz@gmail.com>
2016-10-11 00:34:12 -0400

    get_acls fully recursive

--- src/get_acls.sh
+++ src/get_acls.sh
@@ -19,7 +19,10 @@ if [ -f $CONFIG_FILE ]; then
 	DCOS_IP=$( cat $CONFIG_FILE | jq -r '.DCOS_IP' )
 	ACLS_FILE=$( cat $CONFIG_FILE | jq -r '.ACLS_FILE' )
 	ACLS_PERMISSIONS_FILE=$( cat $CONFIG_FILE | jq -r '.ACLS_PERMISSIONS_FILE' )
-	ACLS_PERMISSIONS_ACTIONS_FILE=$( cat $CONFIG_FILE | jq -r '.ACLS_PERMISSIONS_ACTIONS_FILE' )
+	ACLS_GROUPS_FILE=$( cat $CONFIG_FILE | jq -r '.ACLS_GROUPS_FILE' )
+	ACLS_GROUPS_ACTIONS_FILE=$( cat $CONFIG_FILE | jq -r '.ACLS_GROUPS_ACTIONS_FILE' )
+	ACLS_USERS_FILE=$( cat $CONFIG_FILE | jq -r '.ACLS_USERS_FILE' )
+	ACLS_USERS_ACTIONS_FILE=$( cat $CONFIG_FILE | jq -r '.ACLS_USERS_ACTIONS_FILE' )
 	TOKEN=$(cat $CONFIG_FILE | jq -r '.TOKEN')
 
 else
@@ -45,19 +48,110 @@ echo $ACLS > $ACLS_FILE
 echo "** ACLs: "
 echo $ACLS | jq
 
-#TODO: I'm not getting
-# /acls/{rid}
-# /acls/{rid}/users/{uid}
-# /acls/{rid}/users/{uid}/{action}
-# /acls/{rid}/groups/{gid}
-# /acls/{rid}/groups/{gid}/{action}
+#initialize the file where the permissions will be stored
+touch $ACLS_PERMISSIONS_FILE
+echo "{ "\"array"\": [" > $ACLS_PERMISSIONS_FILE
+
+#loop through the list of ACLs received and get the permissions
 # /acls/{rid}/permissions
+jq -r '.array|keys[]' $ACLS_FILE | while read key; do
 
-#get the list of groups each ACL has
-#get the list of actions each group has
+	echo -e "** DEBUG: Loading ACL "$key" ..."	
+	#extract fields from file
+	_ACL=$( jq ".array[$key]" $ACLS_FILE )
+	_RID=$( echo $_ACL | jq -r ".rid" )
+	echo -e "** DEBUG: RULE "$key" is: "$_GID
+	#get the information of the groups and user memberships of this particular ACL
+	MEMBERSHIPS=$( curl \
+-H "Content-Type:application/json" \
+-H "Authorization: token=$TOKEN" \
+-X GET \
+http://$DCOS_IP/acs/api/v1/acls/$_RID/permissions )
 
-#get the list of users each ACL has
-#get  the list of actions each user has
+	echo -e "** DEBUG: received MEMBERSHIPs are: "$MEMBERSHIPS
+	#Memberships is an array of the different member USERS and GROUPS
+	#loop through both of them.
+	#TODO: change for two-dimensional array instead of nested
+	_GROUPS=$( echo $MEMBERSHIPS | jq -r ".groups" )
+	#loop through the groups and add them to the file
+	echo $_GROUPS | jq -r '.|keys[]' | while read key; do
+
+		_GID=$( echo $_GROUPS | jq -r ".gid" )
+		GROUPURL=$( echo $_GROUPS | jq -r ".groupurl" )
+		#these are the FIELDS of this GROUP (before further arrays)
+		#prepare the body of this GROUP
+		#and leave open for the next array
+		BODY=" { \
+"\"gid"\": "\"$_GID"\",\
+"\"groupurl"\": "\"$GROUPURL"\",
+"\"actions"\": ["
+		#write to the file and continue
+		echo $BODY >> $PERMISSIONS_FILE
+		ACTIONS=$( echo $_GROUPS | jq -r ".actions" )
+		#actions is *YET ANOTHER* array, loop through it etc.
+		#TODO: change for three-dimensional array instead of nested
+		echo $ACTIONS | jq -r '.|keys[]' | while read key; do
+
+			NAME=$( echo $ACTIONS | jq -r ".name" )
+			URL=$( echo $ACTIONS | jq -r ".url" )
+			#prepare body of this ACTION to add it to file
+			BODY=" { \
+"\"name"\": "\"$NAME"\",\
+"\"url"\": "\"$URL"\"\
+},"
+ 			#no deeper arrays so close the JSON
+			#write to the file and continue
+			echo $BODY >> $PERMISSIONS_FILE
+
+		done
+		#close the ACTIONS array, add null last item (comma issue)
+		echo "{} ]," >> $PERMISSIONS_FILE
+	done
+	#close the GROUPS array, add null last item
+	echo "{} ]," >> $PERMISSIONS_FILE
+
+	_USERS=$( echo $MEMBERSHIPS | jq -r ".users" )
+	#loop through the users and add them to the file
+	echo $_USERS | jq -r '.|keys[]' | while read key; do
+
+		_UID=$( echo $_USERS | jq -r ".uid" )
+		USERURL=$( echo $_USERS | jq -r ".userurl" )
+		#these are the FIELDS of this USER (before further arrays)
+		#prepare the body of this USER
+		#and leave open for the next array
+		BODY=" { \
+"\"uid"\": "\"$_UID"\",\
+"\"userpurl"\": "\"$USERURL"\",
+"\"actions"\": ["
+		#write to the file and continue
+		echo $BODY >> $PERMISSIONS_FILE
+		ACTIONS=$( echo $_USERS | jq -r ".actions" )
+		#actions is *YET ANOTHER* array, loop through it etc.
+		#TODO: change for three-dimensional array instead of nested
+		echo $ACTIONS | jq -r '.|keys[]' | while read key; do
+
+			NAME=$( echo $ACTIONS | jq -r ".name" )
+			URL=$( echo $ACTIONS | jq -r ".url" )
+			#prepare body of this ACTION to add it to file
+			BODY=" { \
+"\"name"\": "\"$NAME"\",\
+"\"url"\": "\"$URL"\"\
+},"
+ 			#no deeper arrays so close the JSON
+			#write to the file and continue
+			echo $BODY >> $PERMISSIONS_FILE
+
+		done
+		#close the ACTIONS array, add null last item (comma issue)
+		echo "{} ]," >> $PERMISSIONS_FILE
+	
+	done
+	#close the USERS array, add null last item
+	echo "{} ]," >> $PERMISSIONS_FILE
+
+done
+#close the PERMISSIONS, add null last item
+echo "{} ] }" >> $PERMISSIONS_FILE
 
 #get ACLs_GROUPS
 
